2a53ddedfdc20714d22851b4606404a7755b4f05
SuriyaNitt <suriyanarayanan987@gmail.com>
2017-11-05 02:22:52 -0500

    install OpenCV script cleanup
    Signed-off-by: SuriyaNitt <suriyanarayanan987@gmail.com>

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,9 +1,13 @@
 mkdir opencv
 cd opencv
-wget https://github.com/opencv/opencv/archive/2.4.13.4.zip
-unzip *.zip
+if [ ! -f ./2.4.13.4.zip ]; then
+    wget https://github.com/opencv/opencv/archive/2.4.13.4.zip
+    unzip *.zip
+fi
+cp ../opencv_CMakeLists.txt ./opencv*/
 cd opencv*
 mkdir build
 cd build
+rm -r ./*
 cmake ..
 make -j4
