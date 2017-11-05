4edddd8b6377401782c0b7c09c5b3f0a0da0a2f0
SuriyaNitt <suriyanarayanan987@gmail.com>
2017-11-05 02:31:38 -0500

    install opencv script correction
    Signed-off-by: SuriyaNitt <suriyanarayanan987@gmail.com>

--- install_opencv.sh
+++ install_opencv.sh
@@ -4,7 +4,7 @@ if [ ! -f ./2.4.13.4.zip ]; then
     wget https://github.com/opencv/opencv/archive/2.4.13.4.zip
     unzip *.zip
 fi
-cp ../opencv_CMakeLists.txt ./opencv*/
+cp ../opencv_CMakeLists.txt ./opencv*/CMakeLists.txt
 cd opencv*
 mkdir build
 cd build
